6d31de7da9836548e8883cd2c69bc6e377211462
kmohan321 <kmalphamale@gmail.com>
2025-01-30 14:53:32 +0530

    optimize the kernel

--- day4/vec_dot.cu
+++ day4/vec_dot.cu
@@ -1,31 +1,31 @@
 #include<stdio.h>
 #include<cuda.h>
 
-#define m 24
-#define threads_per_block 24
+#define m 1291
+#define threads_per_block 128
 
 
 //important kernel 
-__global__ void vec_dot(float *v1, float *v2, float *v3){
+__global__ void vec_dot(int *v1, int *v2, int *v3){
       
       int global_idx = threadIdx.x + blockDim.x * blockIdx.x;
       int s_idx = threadIdx.x; //id for thread in a block
 
-      __shared__ float s[threads_per_block]; //storing the partial dot product in shared_memory
-
+      __shared__ int s[threads_per_block]; //storing the partial dot product in shared_memory
+      //(key point whole shared memory is now intialized with int 0)
       if(global_idx < m){
           s[s_idx] = v1[global_idx] * v2[global_idx];
       }
       __syncthreads(); //ensuring each thread has written value in shared_memory
 
       //partial reduction sum for shared_memory 
+      //(works well when each block's shared memory has power of 2 filled positions)
     for(int stride = blockDim.x/2; stride > 0; stride >>= 1){
-        if(s_idx < stride && (global_idx + stride) < m){
+        if(s_idx < stride){
             s[s_idx] += s[s_idx + stride];
         }
+        __syncthreads(); //ensuring that the each threads completes its work
     }
-      __syncthreads(); //ensuring that the each threads completes its work
-
       if (s_idx == 0){ //storing the values from shared memory to the global_memory
         v3[blockIdx.x] = s[0];
       }      
@@ -38,14 +38,14 @@ int main(){
     dim3 grid_size (blocks);
     dim3 block_size (threads_per_block);
 
-    float *vec1 = (float*)malloc(m * sizeof(float));
-    float *vec2 = (float*)malloc(m * sizeof(float));
-    float *vec3 = (float*)malloc( blocks * sizeof(float));
+    int *vec1 = (int*)malloc(m * sizeof(int));
+    int *vec2 = (int*)malloc(m * sizeof(int));
+    int *vec3 = (int*)malloc( blocks * sizeof(int));
 
-    float *p1, *p2, *p3;
-    cudaMalloc((void**)&p1, m * sizeof(float));
-    cudaMalloc((void**)&p2, m * sizeof(float));
-    cudaMalloc((void**)&p3, blocks * sizeof(float));
+    int *p1, *p2, *p3;
+    cudaMalloc((void**)&p1, m * sizeof(int));
+    cudaMalloc((void**)&p2, m * sizeof(int));
+    cudaMalloc((void**)&p3, blocks * sizeof(int));
 
     //intializing the vectors
     for(int i = 0;i<m;i++){
@@ -53,18 +53,18 @@ int main(){
       vec2[i] = i * 2 ;
     }
 
-    cudaMemcpy(p1,vec1,m * sizeof(float),cudaMemcpyHostToDevice);
-    cudaMemcpy(p2,vec2,m * sizeof(float),cudaMemcpyHostToDevice);
+    cudaMemcpy(p1,vec1,m * sizeof(int),cudaMemcpyHostToDevice);
+    cudaMemcpy(p2,vec2,m * sizeof(int),cudaMemcpyHostToDevice);
 
     vec_dot<<<grid_size,block_size>>>(p1,p2,p3);
 
-    cudaMemcpy(vec3,p3,blocks * sizeof(float),cudaMemcpyDeviceToHost);
+    cudaMemcpy(vec3,p3,blocks * sizeof(int),cudaMemcpyDeviceToHost);
     //completing the summation
-    float sum = 0 ;
+    int sum = 0 ;
     for(int i =0; i<blocks; i++){
       sum += vec3[i];
     }
-    printf("the dot product is %.2f",sum);
+    printf("the dot product is %d",sum);
 
     // Free memory
     cudaFree(p1);
