0ee04a863d0b3672255f700154ac49269ca78e3f
kmohan321 <kmalphamale@gmail.com>
2025-03-20 00:22:28 +0530

    updated kl_div

--- day52/kl_div.cu
+++ day52/kl_div.cu
@@ -1,7 +1,12 @@
 #include<stdio.h>
 #include<cuda.h>
 
-__global__ void kl_div(float *P, float *Q, float *loss , int N, int M, float eps){
+
+/*
+here P -> true or target probability (either log_probability or simply p so depends on lop_p)
+Q -> predicted probability (should be log_probability i.e, given as Q = log(q))
+*/
+__global__ void kl_div(float *P, float *Q, float *loss , int N, int M, float eps, bool log_p){
 
   extern __shared__ float smem[];
   int row = blockIdx.x;
@@ -12,7 +17,12 @@ __global__ void kl_div(float *P, float *Q, float *loss , int N, int M, float eps
   for(int i = idx ; i <N; i+=blockDim.x){
       float curr_p = P[row * N + i];
       float curr_q = Q[row *N + i ];
-      local_loss += curr_p * (logf(fmaxf(curr_p , eps)) - log(curr_q));
+      if(!log_p){ //if p is not log(p) i.e, log_p is False
+        local_loss += curr_p * (logf(fmaxf(curr_p , eps)) - curr_q);
+      }
+      else{
+        local_loss += expf(curr_p) * (curr_p - curr_q);
+      }
    }
 
   smem[idx] = local_loss;
@@ -29,6 +39,31 @@ __global__ void kl_div(float *P, float *Q, float *loss , int N, int M, float eps
   }
   
 }
+//computing the gradient with respect to the target
+/*
+grad_loss -> gradient of loss with respect to the output 
+grad -> gradient of loss with respect to the target
+*/
+__global__ void kl_div_back(float *P,float *grad_loss,float *grad,int N, int M, bool log_p){
+
+  extern __shared__ float smem[];
+  int row = blockIdx.x;
+  if(row >= M) return;
+  int idx = threadIdx.x;
+
+  for(int i = idx ; i <N; i+=blockDim.x){
+      float curr_p = P[row * N + i];
+      if(!log_p){ //if p is not log(p) i.e, log_p is False
+        float curr_grad = curr_p * -1.0f;
+        grad[row *N + i] = curr_grad * grad_loss[row * N + i];
+      }
+      else{
+        float curr_grad = -expf(curr_p);
+        grad[row *N + i] = curr_grad * grad_loss[row * N + i];
+      }
+   }
+}
+
 
 
 
