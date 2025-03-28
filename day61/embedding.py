9478b79ff6f2091ae7903c165f44d3735b692ac3
kmohan321 <kmalphamale@gmail.com>
2025-03-29 00:07:27 +0530

    embedding layer

--- day61/embedding.py
+++ day61/embedding.py
@@ -14,20 +14,22 @@ def embedding_forward_kernel(
     pid_m = tl.program_id(0)
     pid_n = tl.program_id(1)
 
-    start_m = pid_m * BLOCK_SIZE_M
-    start_n = pid_n * BLOCK_SIZE_N
-    offsets_m = start_m + tl.arange(0, BLOCK_SIZE_M)
-    mask_m = offsets_m < n_elements
-    indices = tl.load(indices_ptr + offsets_m, mask=mask_m, other=0)
-    offsets_n = start_n + tl.arange(0, BLOCK_SIZE_N)
-    mask_n = offsets_n < embedding_dim
+    start_m = pid_m * BLOCK_SIZE_M # tokens ids are divided in blocks
+    start_n = pid_n * BLOCK_SIZE_N # so it is d dimensional , we are loading a part of it 
+    offsets_m = start_m + tl.arange(0, BLOCK_SIZE_M) # which token ids block we are working on
+    mask_m = offsets_m < n_elements # let say we have n tokens available, think like oupput will (n,d)
+    indices = tl.load(indices_ptr + offsets_m, mask=mask_m, other=0) # so we get block_size_m token ids loaded
+    offsets_n = start_n + tl.arange(0, BLOCK_SIZE_N) #simply loading blocksize_n dimensions
+    mask_n = offsets_n < embedding_dim #not greater than d 
 
-    embedding_offsets = indices[:, None] * embedding_dim + offsets_n[None, :]
+    # shape -> (blocksize_m,blocksize_n)
+    #so what we are doing here is that we have some row indices like [2,5,3] etc
+    # by multiplying with embedding we will reach at that particular row 
+    embedding_offsets = indices[:, None] * embedding_dim + offsets_n[None, :] 
     embeddings = tl.load(
         embeddings_ptr + embedding_offsets,
         mask=mask_m[:, None] & mask_n[None, :],
         other=0.0,
     )
-
     output_offsets = offsets_m[:, None] * embedding_dim + offsets_n[None, :]
     tl.store(output_ptr + output_offsets, embeddings, mask=mask_m[:, None] & mask_n[None, :])
