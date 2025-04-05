e5975086c617aa7a4a3cbac54f713ad9c94b2889
kmohan321 <kmalphamale@gmail.com>
2025-04-06 01:36:36 +0530

    challenge2

--- day69/challenge2.cu
+++ day69/challenge2.cu
@@ -10,36 +10,45 @@ __global__ void cumulative_sum(const float *input, float *out, float *block_sum,
     if(idx < N){
         smem[x] = input[idx];
     }
+    else{
+        smem[x] = 0.0f;
+    }
     __syncthreads();
 
-    for(int stride = 1; stride < blockDim.x/2; stride *=2){
-        if(x>stride){
-            smem[x] += smem[x-stride];
-        }
-        __syncthreads();
-    }
+    for(int stride = 1; stride < blockDim.x; stride *=2){
+        float temp = 0.0f;  
+        if(x >= stride){
+            temp = smem[x] + smem[x-stride]; 
+          }
+          __syncthreads();
+          
+          if(x >= stride){
+            smem[x] = temp;
+          }
+          __syncthreads();
+  
+      }
 
     if(idx<N){
         out[idx] = smem[x];
     }
 
-    if(x==0 && idx < N){
-        block_sum[b_idx] = smem[N-(b_idx * blockDim.x)];
+    if(x==blockDim.x-1 && idx < N){
+        block_sum[b_idx] = smem[x];
     }
 
 }
 
 __global__ void block_level(float *out, float *block_sum, int N){
 
-        int x = threadIdx.x;
-        int b_idx = blockIdx.x;
-        int idx = x + b_idx * blockDim.x;
+    int x = threadIdx.x;
+    int b_idx = blockIdx.x;
+    int idx = x + b_idx * blockDim.x;
 
-        float correction_factor = 0.0f;
-        if(b_idx != 0){
-            correction_factor += block_sum[b_idx-1];
-            out[idx] += correction_factor;
-        }
+    if(b_idx != 0 && idx < N){
+        float correction_factor = block_sum[b_idx-1];
+        out[idx] += correction_factor;
+    }
 
 }
 
@@ -54,6 +63,12 @@ extern "C" void solution(const float* input, float* output, size_t N) {
     cudaMalloc((void**)&d_block_sum , grid.x * sizeof(float));
     int smem_size = threads * sizeof(float);
     cumulative_sum<<<grid,blocksize,smem_size>>>(input, output, d_block_sum,N);
+    float *block_sum = (float*)malloc(grid.x * sizeof(float));
+    cudaMemcpy(block_sum,d_block_sum,grid.x * sizeof(float), cudaMemcpyDeviceToHost);
+    for(int i= 1; i < grid.x; i++){
+        block_sum[i] = block_sum[i-1] + block_sum[i];
+    }
+    cudaMemcpy(d_block_sum,block_sum,grid.x * sizeof(float), cudaMemcpyHostToDevice);
     block_level<<<grid,blocksize>>>(output, d_block_sum,N);
 
 
