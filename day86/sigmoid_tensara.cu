1af1717096d9414f708845115157fef3c9099071
kmohan321 <kmalphamale@gmail.com>
2025-04-23 01:52:20 +0530

    some beautiful chnages

--- day86/sigmoid_tensara.cu
+++ day86/sigmoid_tensara.cu
@@ -3,29 +3,28 @@
 //let's try loading multiple elements
 __global__ void sigmoid(const float * __restrict__ input, float * __restrict__ output, int M, int N){
     
-    int row = blockIdx.x;
-    int idx = threadIdx.x;
-    int row_offset = row * N;
+        int x = threadIdx.x + blockDim.x * blockIdx.x; //col
+        int y = threadIdx.y + blockDim.y * blockIdx.y; //row 
 
-    if(row >= M) return;
-    for(int stride = idx*4; stride < N; stride += blockDim.x*4){
-        float4 *curr_idx = (float4*)&input[row_offset + stride];
-        float4 curr_val = *curr_idx;
-
-        output[row_offset + stride] = 1.0f / (1.0f + __expf(-curr_val.x));
-        output[row_offset + stride] = 1.0f / (1.0f + __expf(-curr_val.y));
-        output[row_offset + stride] = 1.0f / (1.0f + __expf(-curr_val.z));
-        output[row_offset + stride] = 1.0f / (1.0f + __expf(-curr_val.w));
-    }
+        if(y < M && x < N/4){
+            const int idx = y * N/4 + x;
+            float4 curr_val = __ldg((reinterpret_cast<const float4*>(input) + idx));
+            
+            float4 out;
+            out.x = __frcp_rn(1.0f + __expf(-curr_val.x));
+            out.y = __frcp_rn(1.0f + __expf(-curr_val.y));
+            out.z = __frcp_rn(1.0f + __expf(-curr_val.z));
+            out.w = __frcp_rn(1.0f + __expf(-curr_val.w));
+            
+            reinterpret_cast<float4*>(output)[idx] = out;
+         }
 }
-
 
 // Note: input, output are all device pointers to float32 arrays
 extern "C" void solution(const float* input, float* output, size_t n, size_t m) {    
-
-    int threads = 512;
-    dim3 blocksize(threads);
-    dim3 grid (m);
+    int N=n, M=m;
+    dim3 blocksize(32,32);
+    dim3 grid ((N/4 + blocksize.x - 1)/blocksize.x,(M + blocksize.y - 1)/blocksize.y);
     sigmoid<<<grid, blocksize>>>(input,output,m,n);
 
 }
